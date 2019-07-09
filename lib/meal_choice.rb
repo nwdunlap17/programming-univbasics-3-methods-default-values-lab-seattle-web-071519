33562df6dfcf48c50ddc00c081033c241bfbc355
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-09 22:33:19 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,11 @@
-# Your code here
+# Parameters required, default, and optional
+# https://medium.com/podiihq/ruby-parameters-c178fdcd1f4e
+
+
+def meal_choice(veg1,veg2,protein='meat')
+  puts "What a nutritious meal!"
+  output = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts output
+  return output
+end
+
